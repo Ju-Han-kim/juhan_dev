da30807497aedfeb67f71cbfd41a82b0dde0e120
김주한 <@juhan>
2021-03-23 21:06:40 +0900

    FreeBoardMapper 구현

--- FirstWebProject/src/test/java/com/juhan/web/freeBoard/FreeBoardMapperTest.java
+++ FirstWebProject/src/test/java/com/juhan/web/freeBoard/FreeBoardMapperTest.java
@@ -29,8 +29,45 @@ public class FreeBoardMapperTest {
 			System.out.println(article);
 		}
 	}
-
 	
+	//show one article unit test
+	@Test
+	public void getArticle() {
+		System.out.println(mapper.getArticle(1));
+	}
+	
+	//insert article unit test
+	@Test
+	public void insertArticle() {
+		FreeBoardVO article = new FreeBoardVO();
+		
+		article.setTitle("테스트제목");
+		article.setWrtier("테스트작성자");
+		article.setContent("테스트내용");
+		
+		mapper.insertArticle(article);
+	}
+	
+
+	//update article unit test
+	@Test
+	public void updateArticle() {
+		
+		FreeBoardVO article = new FreeBoardVO();
+		
+		article.setBoardNo(1);
+		article.setTitle("수정테스트제목");
+		article.setContent("수정테스트내용");
+		
+		mapper.updateArticle(article);
+		
+	}
+	
+	//delete article unit test
+	@Test
+	public void deleteArticle() {
+		mapper.deleteArticle(1);
+	}
 	
 	
 	
